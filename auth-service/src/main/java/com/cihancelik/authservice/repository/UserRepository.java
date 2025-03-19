fcc0c134abbfb454bbea53d5ed95664e6952b4b4
Cihan Celik <cihan.celik7@gmail.com>
2025-03-19 16:17:34 +0300

    jwt token working correctly. every login success create jwt token

---
+++ auth-service/src/main/java/com/cihancelik/authservice/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.cihancelik.authservice.repository;
+
+import com.cihancelik.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+
+}
